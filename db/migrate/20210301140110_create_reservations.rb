88b6ceee258091a99258f22fb431f74fc56e0869
Ludmila Spee <ludmilaspee@web.de>
2021-03-01 16:49:03 +0100

    Created the model view and controller

---
+++ db/migrate/20210301140110_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :total_price
+      t.datetime :date
+      t.boolean :approved, default: false
+      t.references :portfolio, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
