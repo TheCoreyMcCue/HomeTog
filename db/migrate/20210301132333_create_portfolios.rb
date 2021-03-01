aa585d6768f50a3a3c58b04c1b3f56e3d23bbcdc
RicardoMartinez <ricardoemartine@hotmail.com>
2021-03-01 14:26:20 +0100

    started pundit and generate portfolio model

---
+++ db/migrate/20210301132333_create_portfolios.rb
@@ -0,0 +1,14 @@
+class CreatePortfolios < ActiveRecord::Migration[6.1]
+  def change
+    create_table :portfolios do |t|
+      t.string :name
+      t.string :location
+      t.references :user, null: false, foreign_key: true
+      t.text :description
+      t.integer :price
+      t.string :images
+
+      t.timestamps
+    end
+  end
+end
