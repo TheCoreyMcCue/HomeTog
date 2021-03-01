afb683b8be74b4f6578002da02de81265a5ef81f
Corey McCue <coreysmccue@gmail.com>
2021-03-01 16:47:16 +0100

    create portfolio controller and views

--- app/policies/portfolio_policy.rb
+++ app/policies/portfolio_policy.rb
@@ -4,4 +4,24 @@ class PortfolioPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def show?
+    true
+  end
+
+  def index?
+    true
+  end
 end
