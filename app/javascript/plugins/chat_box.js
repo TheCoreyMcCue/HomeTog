c3855306b80fb955e5789f7b22fc0a731ded2e1e
RicardoMartinez <ricardoemartine@hotmail.com>
2021-03-04 16:03:00 +0100

    implementation of total price in reservationa and layout for chatbox

--- app/javascript/plugins/chat_box.js
+++ app/javascript/plugins/chat_box.js
@@ -1,9 +1,11 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  document.getElementById("chatbox-button").style.display = "none";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  document.getElementById("chatbox-button").style.display = "block";
 }
 
 export { openForm };
