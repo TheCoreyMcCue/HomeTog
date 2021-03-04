c463fa0550bb7fc1bbab02fbdf4954ff1e74ee4a
Corey McCue <coreysmccue@gmail.com>
2021-03-04 16:33:52 +0100

    team bug fix with Malou

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,14 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  has_one :favorites
+  has_many :reservations, dependent: :destroy
 
   def photo
     photos.first
   end
 
-  has_one :favorites
-  has_many :reservations
+
 
   def favorited?(current_user)
     Favorite.find_by(user_id: current_user.id, portfolio_id: id)
