653ec668071be55580066a6d59a0f153576df096
Ludmila Spee <ludmilaspee@web.de>
2021-03-08 15:26:47 +0100

    Create review model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :portfolio
+  has_one :review
+
+  validates :date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_134500) do
+ActiveRecord::Schema.define(version: 2021_03_08_093013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,16 @@ ActiveRecord::Schema.define(version: 2021_03_04_134500) do
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "photographer_description"
+    t.integer "photographer_rating"
+    t.string "user_rating"
+    t.bigint "reservation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -99,4 +109,5 @@ ActiveRecord::Schema.define(version: 2021_03_04_134500) do
   add_foreign_key "portfolios", "users"
   add_foreign_key "reservations", "portfolios"
   add_foreign_key "reservations", "users"
+  add_foreign_key "reviews", "reservations"
 end
