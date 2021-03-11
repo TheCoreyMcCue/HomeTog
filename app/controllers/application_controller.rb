1522ee220363452298837770f85beca2fe974c12
Ludmila Spee <ludmilaspee@web.de>
2021-03-11 12:14:34 +0100

    Added favicon adn metatext

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,11 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  #override Rails.application.default_url_options[:host]
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
